11e25ce7ff88d7f6fbdf72586fdfac6f7d654c98
Jaguar16 <aymen.baroudi@etu.unice.fr>
2020-02-24 19:26:24 +0100

    Ajout des classes Serveur et UE, le serveur démarre

--- java/serveur/src/main/java/matière/UE.java
+++ java/serveur/src/main/java/matière/UE.java
@@ -1,4 +1,43 @@
 package matière;
 
 public class UE {
+    private final String code;
+    private final String discipline;
+    private final int semestre;
+    private final int ECTS;
+    private final String nomUE;
+    private final int nbPlaces;
+
+    public UE(String code, String discipline, String nomUE, int semestre, int ECTS, int nbPlaces) {
+        this.code = code;
+        this.discipline = discipline;
+        this.semestre = semestre;
+        this.ECTS = ECTS;
+        this.nomUE = nomUE;
+        this.nbPlaces = nbPlaces;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDiscipline() {
+        return discipline;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public int getECTS() {
+        return ECTS;
+    }
+
+    public String getNomUE() {
+        return nomUE;
+    }
+
+    public int getNbPlaces() {
+        return nbPlaces;
+    }
 }
--- java/serveur/src/main/java/serveur/Serveur.java
+++ java/serveur/src/main/java/serveur/Serveur.java
@@ -1,4 +1,60 @@
 package serveur;
 
+
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.DataListener;
+import matière.UE;
+
+import java.util.ArrayList;
+
 public class Serveur {
+
+    private final SocketIOServer server;
+    private final ArrayList<UE> listUE;
+
+
+    public Serveur(SocketIOServer server, ArrayList<UE> listUE) {
+        this.server = server;
+        this.listUE = listUE;
+
+        this.server.addEventListener("touche", String.class, new DataListener<String>() {
+            @Override
+            public void onData(SocketIOClient socketIOClient, String s, AckRequest ackRequest) throws Exception {
+                traitement(socketIOClient);
+            }
+        });
+    }
+
+    protected void traitement(SocketIOClient socketIOClient) {
+        System.out.println("Le client clique sur Semestre 1");
+    }
+
+
+    public static final void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.setHostname("127.0.0.1");
+        configuration.setPort(4444);
+
+        /*Création du serveur*/
+        SocketIOServer server = new SocketIOServer(configuration);
+
+        Serveur serveur = new Serveur(server, Serveur.initListeUE());
+        server.start();
+
+
+    }
+
+    /*initialisation des matières et de la liste des matières*/
+    private static ArrayList<UE> initListeUE(){
+        UE ueMath1 = new UE("SPUM13", "MATHS", "Complément 1", 1,6,100);
+        UE ueMath2 = new UE("SPUM12", "MATHS", "Méthodes - approche continue", 1,6,280);
+        ArrayList<UE> listUE = new ArrayList<>();
+        listUE.add(ueMath1);
+        listUE.add(ueMath2);
+        return listUE;
+    }
+
 }
