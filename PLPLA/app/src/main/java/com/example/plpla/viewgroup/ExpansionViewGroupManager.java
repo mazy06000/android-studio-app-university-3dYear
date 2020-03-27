c1c9ea1d8190046dda6cd9144e2ea61fb9a21615
Mohamed Mazy <mazymohamed06@gmail.com>
2020-03-27 12:41:23 +0100

    Ajout des classes manquants fixes #79

---
+++ PLPLA/app/src/main/java/com/example/plpla/viewgroup/ExpansionViewGroupManager.java
@@ -0,0 +1,34 @@
+package com.example.plpla.viewgroup;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.plpla.ExpansionLayout;
+import com.example.plpla.viewgroup.ExpansionLayoutCollection;
+
+class ExpansionViewGroupManager {
+    private final ViewGroup viewGroup;
+    private com.example.plpla.viewgroup.ExpansionLayoutCollection expansionLayoutCollection = new ExpansionLayoutCollection();
+
+    public ExpansionViewGroupManager(ViewGroup viewGroup) {
+        this.viewGroup = viewGroup;
+    }
+
+    public void onViewAdded(){
+        findExpansionsViews(viewGroup);
+    }
+
+    private void findExpansionsViews(View view){
+        if(view instanceof ExpansionLayout){
+            expansionLayoutCollection.add((ExpansionLayout) view);
+        } else if(view instanceof ViewGroup){
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                findExpansionsViews(((ViewGroup) view).getChildAt(i));
+            }
+        }
+    }
+
+    public void setOpenOnlyOne(boolean openOnlyOne) {
+        this.expansionLayoutCollection.openOnlyOne(openOnlyOne);
+    }
+}
