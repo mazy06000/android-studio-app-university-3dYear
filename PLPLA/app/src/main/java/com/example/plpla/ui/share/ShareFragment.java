894a88aa66d2ab088e9b6a3a8922298626d924ff
Said Elarays <saidelarays@hotmail.fr>
2020-04-17 21:14:00 +0200

    test basique de partage parcours fixes #121

--- PLPLA/app/src/main/java/com/example/plpla/ui/share/ShareFragment.java
+++ PLPLA/app/src/main/java/com/example/plpla/ui/share/ShareFragment.java
@@ -5,21 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.plpla.R;
-import com.example.plpla.edit;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class ShareFragment extends Fragment {
 
